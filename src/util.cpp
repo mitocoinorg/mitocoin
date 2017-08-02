6a47521d154e21bcd9c19909adc2bf4bd102fe0b
freeman <xyusto@gmail.com>
2017-08-02 12:25:16 -0700

    fixing details

--- src/util.cpp
+++ src/util.cpp
@@ -801,12 +801,14 @@ int GetNumCores()
 
 std::string CopyrightHolders(const std::string& strPrefix)
 {
-    std::string strCopyrightHolders = strPrefix + strprintf(_(COPYRIGHT_HOLDERS), _(COPYRIGHT_HOLDERS_SUBSTITUTION));
+    std::string strYearMito = strPrefix;
+    strYearMito.replace(strYearMito.find("2017-2017"), sizeof("2017-2017")-1, "2017");
+    std::string strCopyrightHolders = strYearMito + strprintf(_(COPYRIGHT_HOLDERS), _(COPYRIGHT_HOLDERS_SUBSTITUTION));
 
     // Check for untranslated substitution to make sure Bitcoin Core copyright is not removed by accident
     if (strprintf(COPYRIGHT_HOLDERS, COPYRIGHT_HOLDERS_SUBSTITUTION).find("Bitcoin Core") == std::string::npos) {
         std::string strYear = strPrefix;
-        strYear.replace(strYear.find("2011"), sizeof("2011")-1, "2009");
+         strYear.replace(strYear.find("2017"), sizeof("2017")-1, "2011");
         strCopyrightHolders += "\n" + strYear + "The Bitcoin Core developers";
     }
     return strCopyrightHolders;
